da11ee23054b9b6f6813fd44bf8836d8a5f563c3
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2024-01-24 18:11:13 -0500

    rethinking the data

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,6 @@
 const router = require('express').Router()
+const places = require('../models/places')
+
 router.get ('/new', (req, res) => {
     res.render('places/new')
 })
---
+++ models/places.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    name: 'Yard House',
+    city: 'Raleigh',
+    state: 'NC',
+    cuisines: 'American',
+    pic: '/public/images/yard-house-tables.jpg'
+}, {
+    name: 'Happy Coffee',
+        city: 'Chicago',
+        state: 'IL',
+        cuisines: 'Coffee, Bakery',
+        pic: '/public/images/coffee-shop.jpg'
+
+}]
