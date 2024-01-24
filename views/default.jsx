0c1341816131065529a5fd6dd5938ca1a6946736
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2024-01-24 17:45:12 -0500

    included bootstrap in css

--- views/default.jsx
+++ views/default.jsx
@@ -6,7 +6,7 @@ function Def (html) {
             <head>
                 <title>Title</title>
                 {/* BOOTSTRAP CDN LINK (long) */}
-                <link rel="stylesheet" href="/css/style.css"/>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
             </head>
             <body>
                 {html.children}
