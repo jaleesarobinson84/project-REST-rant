1951b8ab74fd6cbc867185cd2dadf8af855d5164
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2024-01-23 19:28:13 -0500

    making a form view

---
+++ views/places/new.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+const Def = require('../default')
+
+function new_form () {
+    return (
+        <Def>
+            <main>
+                <h1>Add a NEW PLACE</h1>
+            </main>
+        </Def>
+    )
+
+
+}
+
+module.exports = new_form
