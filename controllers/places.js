b715b41a4c12a14e4df5ad4d9b7ea611cfe13549
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2024-01-23 19:36:05 -0500

    updating the new route

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
+router.get('/new', (req, res) => {
     let places = [{
         name: 'Yard House',
         city: 'Raleigh',
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
         pic: 'http://placekitten.com/250/250'
 
     }]
-    res.render('places/index', {places})
+    res.render('places/new')
 })
 
 module.exports = router
