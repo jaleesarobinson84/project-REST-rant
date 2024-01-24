a85dd1400d22c52dbe4272d9131640bfd564610a
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2024-01-24 10:03:16 -0500

    fill in index page

--- controllers/places.js
+++ controllers/places.js
@@ -8,13 +8,13 @@ router.get('/', (req, res) => {
         city: 'Raleigh',
         state: 'NC',
         cuisines: 'American',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/public/images/yard-house-tables.jpg'
     }, {
         name: 'Happy Coffee',
         city: 'Chicago',
         state: 'IL',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/public/images/coffee-shop.jpg'
 
     }]
     res.render('places/index', {places}) 
