2c689994da276618c3be2138cba0f7d5b615c04e
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2024-01-24 18:23:59 -0500

    add place

--- controllers/places.js
+++ controllers/places.js
@@ -22,9 +22,21 @@ router.get('/', (req, res) => {
     res.render('places/index', {places}) 
 })
 
-router.post('/', (req, res) =>{
+router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places stub')
-})
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
 
 module.exports = router
