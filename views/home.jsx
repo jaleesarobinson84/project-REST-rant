7ea4cdb2b224ebbd7b3150a0a5ddbba7f6222234
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2024-01-24 09:19:11 -0500

    image to home view

--- views/home.jsx
+++ views/home.jsx
@@ -6,11 +6,18 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
-            </main>
+                <div>
+                    <img src="/public/images/pancakes-and-fruit.jpg" alt="pancake" />
+                <div>
+                Photo by <a href="https://unsplash.com/@moniqa?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Monika Grabkowska</a> on <a href="https://unsplash.com/photos/pancake-with-orange-and-blueberries-beside-scattered-chocolate-and-coffee-beans-P1aohbiT-EY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+            </div>
+        </div>
+            
         <a href="/places">
     <button className="btn-primary">Places Page</button>
 
 </a>
+</main>
         </Def>
     )
 }
