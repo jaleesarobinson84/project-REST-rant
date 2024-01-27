92f0068f22fb24edd2e661562280214b9dd524bf
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2024-01-27 18:50:02 -0500

    connect the show route

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,20 @@
 const router = require('express').Router()
 const places = require('../models/places')
 
-router.get ('/new', (req, res) => {
-    res.render('places/new')
+// SHOW
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+  res.render('places/show', {place: places[id]})
+  }
 })
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'Yard House',
