3b8c7de7ca1a846c2df98210a5fcef564a2f527f
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2024-01-27 19:59:59 -0500

    Delete functionality implemnted

--- controllers/places.js
+++ controllers/places.js
@@ -15,6 +15,21 @@ router.get('/:id', (req, res) => {
   }
 })
 
+// DELETE
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else { 
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'Yard House',
