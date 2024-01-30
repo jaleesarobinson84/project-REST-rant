59d769ae0b65ea14faaf2a4a91b7e35c2331276f
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2024-01-29 19:32:16 -0500

    add the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -41,6 +41,7 @@ router.get('/:id/edit', (req, res) => {
   }
 })
 
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'Yard House',
@@ -64,21 +65,33 @@ router.get('/', (req, res) => {
     res.render('places/index', {places}) 
 })
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    if (!req.body.pic) {
-      // Default image if one is not provided
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-      req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('/places')
-  })
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = '/images/yard-house-tables.jpg'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`);
+  }
+})
+
   
 
 module.exports = router
